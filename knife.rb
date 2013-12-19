cec595baf7e43ff7b89127a1e6f80edc4baf6b25
Daniel Jabbour <daniel@amoe.ba>
2013-12-18 16:27:26 -0800

    Adding environments to the mix (new Knife feature)

--- knife.rb
+++ knife.rb
@@ -2,4 +2,5 @@ base = File.expand_path('..', __FILE__)
 node_path                 File.join(base, 'nodes')
 role_path                 File.join(base, 'roles')
 data_bag_path             File.join(base, 'data_bags')
+environment_path          File.join(base, 'environments')
 cookbook_path             %w(site-cookbooks cookbooks).map {|d| File.join(base, d)}
